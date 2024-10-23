00377f19695fa7094dca55d833f36f0bb5a4d0c4
Jenil <43415449+AgentHackerYT@users.noreply.github.com>
2024-10-23 12:30:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -164,7 +164,7 @@ app.get("/1138894.txt", (req, res) =>{
 
 app.get("/1269518.sw.js", (req,res)=>{
       res.setHeader("Content-Type", "text/javascript")
-        res.send("self.opts = {zoneID: 1269518,swDomain: "push-sdk.com",}\nimportScripts('https://push-sdk.com/f/sw.js')")  
+        res.send("self.opts = {zoneID: 1269518,swDomain: \"push-sdk.com\",}\nimportScripts('https://push-sdk.com/f/sw.js')")  
 })
 
 app.listen(8080)
